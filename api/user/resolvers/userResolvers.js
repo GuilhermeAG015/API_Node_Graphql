dd555f96282f1c0dcb7b6245e2499f6ce32308fa
Guilherme Grilo <guilhermegrilo2009@hotmail.com>
2021-09-20 17:40:40 -0300

    :construction: WIP Integrations with json-server

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,18 +1,6 @@
-const arrayusers = [
-    {
-        name: 'Guilherme',
-        active: true
-    },
-    {
-        name: 'Alexandre',
-        active: false
-    }
-]
-
 const userResolvers = {
     Query: {
-        users: () => arrayusers,
-        primeiroUser: () => arrayusers[0]
+        users: (root, args, { dataSources }) => dataSources.UserAPI.getUsers() 
     }
 }
 
