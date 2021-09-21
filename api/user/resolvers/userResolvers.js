467e128e4eb24eb6974f3d3100916e506a38121f
Guilherme Grilo <guilhermegrilo2009@hotmail.com>
2021-09-21 16:18:12 -0300

    :sparkles: Adding mutations and new data in data.json archive

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -2,6 +2,12 @@ const userResolvers = {
     Query: {
         users: (root, args, { dataSources }) => dataSources.userAPI.getUsers(),
         user: (root, { id }, { dataSources } ) => dataSources.userAPI.getUserById(id)
+    },
+
+    Mutation: {
+        addUser: async (root, user, { dataSources }) => dataSources.userAPI.addUser(user),
+        refreshUser: async ( root, newData, { dataSources } ) => dataSources.userAPI.refreshUser(newData),
+        deleteUser: async (root, { id } , { dataSources }) => dataSources.userAPI.deleteUser(id) 
     }
 }
 
